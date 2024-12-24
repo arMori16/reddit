c22c3a793491da638181bc2af31eb12b05f7716f
Bogdan Masalev <armori@Bogdans-MacBook-Air.local>
2024-12-24 16:14:25 +0200

    Fixed crucial bugs

--- src/app/(admin)/admin/series/view/[seriesName]/page.tsx
+++ src/app/(admin)/admin/series/view/[seriesName]/page.tsx
@@ -10,7 +10,7 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
     const [genreItems,setGenreItems] = useState<number>(0);
     const [voiceItem,setVoiceItems] = useState<number>(0);
 
-    const {register,handleSubmit,reset,control} = useForm<SeriesInfo>({
+    const {register,handleSubmit,reset,watch,getValues} = useForm<SeriesInfo>({
         defaultValues:{
             seriesName: data?.seriesName,
             description: data?.description,
@@ -28,7 +28,6 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
     })
     
     
-    
     const handleDataSubmit = (data:SeriesInfo)=>{
         console.log(`DATA submit: `,data);
         
@@ -43,17 +42,18 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
         if (data && Array.isArray(data[type])) {
             // Create a new array with the item removed
             const newArray = data[type].filter((_, i) => i !== index);
-    
+            //GetValues
+            const currentData = getValues();
             // Update the 'data' state with the new array
             setData((prev) => prev ? { ...prev, [type]: newArray } : undefined);
     
             // Optionally, reset the form with the updated 'data'
-            reset({ ...data, [type]: newArray });
+            reset({ ...currentData, [type]: newArray });
             console.log('GOOD NIGHT!');
             
         }
     };
-
+    const formValues = watch(); 
     useEffect(()=>{
         const fetchData = async()=>{
 
@@ -76,7 +76,7 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
                             <img className='flex max-h-full w-full rounded-[20px]' src={`http://localhost:3001/media/images/${params.seriesName}/images`} alt={data.seriesName}/>
                         </div>
                         <div className='flex flex-col min-h-[22rem]'>
-                            <textarea className='text-3xl custom-xs:mt-0 flex bg-transparent min-h-[2.5rem]' defaultValue={data.seriesViewName} {...register('seriesName')}></textarea>
+                            <textarea className='text-3xl custom-xs:mt-0 flex bg-transparent min-h-[2.5rem]' defaultValue={data.seriesViewName} {...register('seriesViewName')}></textarea>
                             <ul className='flex flex-col gap-y-1 mt-3'>
                                 <li className="flex">
                                     <div className='w-[6rem]'>Status:</div>
@@ -91,7 +91,7 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
                                     <div className='w-[6rem]'>Genre:</div> 
                                     <div className='flex gap-1 ml-5 w-[24rem] flex-wrap'>{data.genre.map((item:string,index:number)=>(
                                         <div key={index} className="flex border-2 border-gray-500 bg-transparent rounded-md px-1 pl-[6px]">
-                                            <textarea defaultValue={item} {...register(`genre.${index}`)} className={`flex bg-transparent hover:border-rose-50 rounded-md font-medium text-[0.85rem] py-[2px] items-center mr-1 justify-center`}>
+                                            <textarea defaultValue={item}  {...register(`genre.${index}`)} className={`flex bg-transparent hover:border-rose-50 rounded-md font-medium min-w-[2.5rem] text-[0.85rem] py-[2px] items-center mr-1 justify-center`}>
                                                 
                                             </textarea>
                                             <div className="flex justify-center items-center">
@@ -101,12 +101,17 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
                                             </div>
                                         </div>
                                         ))}
-                                        {Array.from({length:genreItems},(_,index)=>(
-                                            <textarea {...register(`genre.${data.genre.length + index}`)} key={index} className='flex border-2 border-gray-500 bg-transparent min-w-[3.5rem] hover:border-rose-50 rounded-md font-medium text-[0.85rem] py-[2px] px-[4px] items-center justify-center'>
+                                        {/* {Array.from({length:genreItems},(_,index)=>(
+                                            <textarea {...register(`genre.${data.genre.length + index}`)} key={data.genre.length + index} className='flex border-2 border-gray-500 bg-transparent min-w-[3.5rem] hover:border-rose-50 rounded-md font-medium text-[0.85rem] py-[2px] px-[4px] items-center justify-center'>
                                                 
                                             </textarea>
-                                        ))}
-                                        <button type="button" onClick={()=>setGenreItems((prev)=>prev + 1)} className="flex h-[1.75rem] text-white rounded-md px border-gray-500 border-2  px-[6px] items-center justify-center">
+                                        ))} */}
+                                        <button type="button" onClick={()=>{/* setGenreItems((prev)=>prev + 1); */
+                                            const currentValues = getValues();
+                                            const updatedGenre:string[] = [...currentValues.genre,""];
+                                            setData((prev) => (prev?{...data,genre:updatedGenre}:undefined));
+                                            reset({...currentValues,genre:updatedGenre});
+                                        }} className="flex h-[1.75rem] text-white rounded-md px border-gray-500 border-2  px-[6px] items-center justify-center">
                                             +
                                         </button>
                                     </div>
@@ -123,7 +128,7 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
                                     <div className='w-[6rem]'>Voice:</div>
                                     <div className='ml-5 flex gap-1 w-[17rem] flex-wrap'>{data.voiceActing.map((item:string,index:number)=>(
                                         <div key={index} className="flex items-center bg-[#4eb997] hover:bg-[#43a083] rounded-md px-1 pl-[6px]">
-                                            <textarea defaultValue={item} {...register(`voiceActing.${index}`)} className='flex h-[1.50rem] bg-transparent rounded-md font-medium text-[0.85rem] py-[2px] px-[2px] items-center justify-center'>
+                                            <textarea defaultValue={item} {...register(`voiceActing.${index}`)} className='flex h-[1.50rem] min-w-[2.5rem] bg-transparent rounded-md font-medium text-[0.85rem] py-[2px] px-[2px] items-center justify-center'>
                                                 
                                             </textarea>
                                             <div className="flex justify-center items-center">
@@ -133,12 +138,17 @@ const ViewSeries = ({params}:{params:{seriesName:string}})=>{
                                             </div>
                                         </div>
                                         ))}
-                                        {Array.from({length:voiceItem},(_,index)=>(
+                                        {/* {Array.from({length:voiceItem},(_,index)=>(
                                             <textarea {...register(`voiceActing.${data.voiceActing.length + index}`)} key={index} className='flex h-[1.50rem] bg-[#4eb997] hover:bg-[#43a083] min-w-[3.5rem] rounded-md font-medium text-[0.85rem] py-[2px] px-[4px] items-center justify-center'>
                                                 
                                             </textarea>
-                                        ))}
-                                        <button type="button" onClick={()=>setVoiceItems((prev)=>prev + 1)} className="flex h-[1.75rem] text-white rounded-md px border-gray-500 border-2  px-[6px] items-center justify-center">
+                                        ))} */}
+                                        <button type="button" onClick={()=>{
+                                            const currentValues = getValues();
+                                            const updatedVoice = [...currentValues.voiceActing,""];
+                                            setData((prev)=>(prev?{...prev,voiceActing:updatedVoice}:undefined));
+                                            reset({...currentValues,voiceActing:updatedVoice});
+                                        }} className="flex h-[1.75rem] text-white rounded-md px border-gray-500 border-2  px-[6px] items-center justify-center">
                                             +
                                         </button>
                                     </div>
